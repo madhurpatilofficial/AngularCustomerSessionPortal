747513659d86d7300bdfaa29397f73f9f0147906
Madhur Dhondu Patil <madhurd@maveric-systems.com>
2023-12-26 18:34:18 +0530

    header

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,31 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+ 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+ 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    });
+ 
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
-
+ 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
-
-  it(`should have as title 'CustomerSessionPortal'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('CustomerSessionPortal');
+ 
+  it('should have a valid title', () => {
+    expect(component.title).toBeTruthy();
+    expect(typeof component.title).toBe('string');
+    expect(component.title.length).toBeGreaterThan(20);
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('CustomerSessionPortal app is running!');
+ 
+  it('should have the correct title', () => {
+    expect(component.title).toEqual('CustomerSessionPortal');
   });
+ 
 });
